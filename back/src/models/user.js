2eaa19263bd43ab0ffb969aba2e63d34ba4d6a80
Ismael.CEREZO <ismaelcerezo2002@gmail.com>
2026-03-13 10:56:37 +0100

    Refactor user schema by removing unnecessary comments and cleaning up the code structure.

--- back/src/models/user.js
+++ back/src/models/user.js
@@ -26,9 +26,6 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
- main
-  },
-=======
     toJSON: {
       virtuals: true,
       versionKey: false,
@@ -50,7 +47,6 @@ const userSchema = new mongoose.Schema(
       },
     },
   }
- model-user
 );
 
 export const User = mongoose.model("User", userSchema);
